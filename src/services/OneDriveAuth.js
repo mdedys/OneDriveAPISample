bd302964a50caee8256dbc7fd4776110e427fd14
Mike Dedys <michal.dedys@D2L.com>
2017-06-08 16:30:56 -0400

    Fixed redirect url

--- src/services/OneDriveAuth.js
+++ src/services/OneDriveAuth.js
@@ -41,7 +41,7 @@ function challengeForAuth() {
         const appInfo = {
             clientId: '79878dd4-12f4-48ff-9267-904c4ef43624',
             scopes: 'user.read files.read files.read.all files.readwrite.all',
-            redirectUri: 'http://mdedys.github.io/OneDriveAPISample/callback.html',
+            redirectUri: 'https://mdedys.github.io/OneDriveAPISample/callback.html',
             authServiceUri: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
         };
 
